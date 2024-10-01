d84e8c402a6586832a27b76914d524f370b8e072
Nicholas Landry <nicholas.landry.91@gmail.com>
2024-10-01 13:52:58 -0400

    Update scripts

--- empirical_simpliciality.py
+++ empirical_simpliciality.py
@@ -25,26 +25,22 @@ datasets = [
 
 min_size = 2
 
-try:
-    with open(f"Data/empirical_simpliciality.json", "r") as file:
-        data = json.loads(file.read())
-except:
-    data = dict()
+data = dict()
 
-    # This argument is very important to the computational feasibility of this algorithm
-    max_order = 10
+# This argument is very important to the computational feasibility of this algorithm
+max_order = 10
 
-    for d in datasets:
-        data[d] = dict()
-        H = xgi.load_xgi_data(d, max_order=max_order)
-        H.cleanup(singletons=True)
+for d in datasets:
+    data[d] = dict()
+    H = xgi.load_xgi_data(d, max_order=max_order)
+    H.cleanup(singletons=True)
 
-        data[d]["es"] = edit_simpliciality(H, min_size=min_size)
-        data[d]["fes"] = face_edit_simpliciality(H, min_size=min_size)
-        data[d]["sf"] = simplicial_fraction(H, min_size=min_size)
+    data[d]["es"] = edit_simpliciality(H, min_size=min_size)
+    data[d]["fes"] = face_edit_simpliciality(H, min_size=min_size)
+    data[d]["sf"] = simplicial_fraction(H, min_size=min_size)
 
-        print("Just finished ", d, flush=True)
+    print("Just finished ", d, flush=True)
 
-    with open(f"Data/empirical_simpliciality.json", "w") as file:
-        datastring = json.dumps(data, indent=2)
-        file.write(datastring)
+with open(f"Data/empirical_simpliciality.json", "w") as file:
+    datastring = json.dumps(data, indent=2)
+    file.write(datastring)
